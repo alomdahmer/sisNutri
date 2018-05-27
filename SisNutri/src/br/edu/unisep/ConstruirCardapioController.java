0ab82746e6abe00994dd73cfc3ba491ad1db8eef
Alom Dahmer <Alom Dahmer@LAPTOP-6FLVISI4>
2018-05-27 17:46:07 -0300

    Versão IV

--- SisNutri/src/br/edu/unisep/ConstruirCardapioController.java
+++ SisNutri/src/br/edu/unisep/ConstruirCardapioController.java
@@ -7,9 +7,12 @@ import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
 import br.edu.unisep.fx.annotation.OnlyNumber;
 import br.edu.unisep.fx.controller.ModalController;
+import br.edu.unisep.model.dao.AlimentoDAO;
 import br.edu.unisep.model.dao.NutricionistaDAO;
 import br.edu.unisep.model.dao.PacienteDAO;
 import br.edu.unisep.model.vo.AlimentoVO;
+import br.edu.unisep.model.vo.CardapioVO;
+import br.edu.unisep.model.vo.AlimentoCardapioVO;
 import br.edu.unisep.model.vo.NutricionistaVO;
 import br.edu.unisep.model.vo.PacienteVO;
 import javafx.collections.FXCollections;
@@ -17,6 +20,11 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 
 public class ConstruirCardapioController extends ModalController {
 
@@ -24,6 +32,7 @@ public class ConstruirCardapioController extends ModalController {
 	@FXML private JFXComboBox<NutricionistaVO> cmbNutricionista;
 	@FXML private JFXDatePicker dtdata;
 	
+	@FXML private JFXTextField txtnome;
 	@OnlyNumber(isDecimal=true, allowsNegative=false, decimalCount=2)
 	@FXML private JFXTextField txtenergia;
 	@OnlyNumber(isDecimal=true, allowsNegative=false, decimalCount=2)
@@ -51,17 +60,25 @@ public class ConstruirCardapioController extends ModalController {
 	
 	private ObservableList<PacienteVO> pacientes;
 	private ObservableList<NutricionistaVO> nutricionistas;
+	private ObservableList<AlimentoVO> alimentos;
+	private ObservableList<AlimentoCardapioVO> alimentoCardapio;
+	
+	CardapioVO cardapio = new CardapioVO();
 	
 	@Override
 	protected void onModalInit() {
 		pacientes = FXCollections.observableArrayList();
 		nutricionistas = FXCollections.observableArrayList();
+		alimentos = FXCollections.observableArrayList();
 		List<PacienteVO> paciente = listarPaciente();
 		List<NutricionistaVO> nutricionista = listarNutricionista();
+		List<AlimentoVO> alimento = listarAlimentos();
 		pacientes.setAll(paciente);
 		cmbPaciente.setItems(pacientes);
 		nutricionistas.setAll(nutricionista);
 		cmbNutricionista.setItems(nutricionistas);
+		alimentos.setAll(alimento);
+		listAlimento.setItems(alimentos);
 	}
 	
 	private List<PacienteVO> listarPaciente(){
@@ -74,6 +91,12 @@ public class ConstruirCardapioController extends ModalController {
 		NutricionistaDAO dao = new NutricionistaDAO();
 		List<NutricionistaVO> nutricionista = dao.listar();
 		return nutricionista;
+	}
+	
+	private List<AlimentoVO> listarAlimentos(){
+		AlimentoDAO dao = new AlimentoDAO();
+		List<AlimentoVO> alimentos = dao.listar();
+		return alimentos;
 	}
 	
 	public void limparTela() {
@@ -94,6 +117,34 @@ public class ConstruirCardapioController extends ModalController {
 		lblLipidios.setText("");
 		lblProteinas.setText("");
 		lblAlerta.setText("");
+	}
+	
+	public void dragStart(MouseEvent event) {
+		Dragboard db = listAlimento.startDragAndDrop(TransferMode.MOVE);
+		ClipboardContent content = new ClipboardContent();
+		content.putString(String.valueOf(listAlimento.getSelectionModel().getSelectedIndex()));
+		db.setContent(content);
+		event.consume();
+	}
+	
+	public void dragOver(DragEvent event) {
+		event.acceptTransferModes(TransferMode.MOVE);
+		event.consume();
+	}
+	
+	public void dragDrop(DragEvent event) {
+		Dragboard db = event.getDragboard();
+		int pos = Integer.parseInt(db.getString());
+		AlimentoVO alimento = alimentos.remove(pos); //remoção ocorre a partir do observableList
+		
+		//Cria um objeto ExercícioVO para associar o exercício ao treino
+		AlimentoCardapioVO aliCard = new AlimentoCardapioVO();
+		aliCard.setAlimento(alimento);
+		aliCard.setCardapio(cardapio);
+		
+		alimentoCardapio.add(aliCard);
+		cardapio.getAlimentos().add(aliCard);
+		openModal("IncluirGramatura.fxml", null);
 	}
 	
 	public void cancelar() {
--- SisNutri/src/br/edu/unisep/IncluirGramaturaController.java
+++ SisNutri/src/br/edu/unisep/IncluirGramaturaController.java
@@ -1,5 +1,40 @@
 package br.edu.unisep;
 
-public class IncluirGramaturaController {
+import com.jfoenix.controls.JFXTextField;
 
+import br.edu.unisep.fx.controller.ModalController;
+import br.edu.unisep.model.vo.AlimentoCardapioVO;
+import javafx.fxml.FXML;
+
+public class IncluirGramaturaController extends ModalController {
+	@FXML private JFXTextField txtGramatura;
+	private AlimentoCardapioVO origem;
+
+	@Override
+	protected void onModalInit() {
+		
+	}
+	
+	private AlimentoCardapioVO retornar(AlimentoCardapioVO alimento) {
+		
+		
+		return alimento; 
+	}
+
+	public AlimentoCardapioVO getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(AlimentoCardapioVO origem) {
+		this.origem = origem;
+	}
+
+	public JFXTextField getTxtGramatura() {
+		return txtGramatura;
+	}
+
+	public void setTxtGramatura(JFXTextField txtGramatura) {
+		this.txtGramatura = txtGramatura;
+	}
+	
 }
